c1efe4bd9569908837fd7b5adf2c56bf7e8ba448
FlozyTess <cecilrency23@gmail.com>
2025-03-16 18:55:19 +0300

    implemented teach() to return a random knowledge string

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from user import User
+import random
 from user import User
 class Teacher(User):
     def __init__(self, first_name, last_name):
@@ -17,4 +17,5 @@ class Teacher(User):
         ]
 
     def teach(self):
+        return random.choice(self.knowledge)
         pass
